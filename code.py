1495201a791764e7edb7f2c51c7c71ead5e1141d
karthik1705 <saikarthik.ch1705@gmail.com>
2025-01-28 16:40:45 -0600

    Added data and requirements.txt

---
+++ code.py
@@ -0,0 +1,14 @@
+#%%
+import pandas as pd
+import numpy as np
+import torch
+import transformers
+import accelerate
+import datasets
+import evaluate
+import numpy
+import pandas
+import tqdm
+import wandb
+import sentencepiece
+# %%
